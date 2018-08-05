44615ceb17299c28a79d5109b1747e50f2ecbd39
BarAzulay <41300710+BarAzulay@users.noreply.github.com>
2018-08-05 13:28:38 +0300

    Add files via upload

---
+++ nutrition/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.db.models import Sum
+
+
+User = get_user_model()
+
+
+class Plan(models.Model):
+
+    class Meta:
+        ordering = ['date']
+
+    user = models.ForeignKey(User, related_name="nutrition", on_delete=models.CASCADE)
+    subtitle = models.CharField(max_length=40)
+    date = models.DateField(name='date', auto_now_add=True)
+
+    def __str__(self):
+        return self.subtitle
+
+    def get_absolute_url(self):
+        '''
+        Returns the canonical URL to view this object
+        '''
+        return reverse('nutrition:detail', kwargs={'plan_id': self.id})
+    @staticmethod
+    def get_energy_value():
+
+        x = Nutrition.get_energy_value()
+        return x
+
+class Nutrition(models.Model):
+
+    class Meta:
+        ordering = ['time']
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    time = models.TimeField(null=True, blank=True)
+    description = models.TextField()
+    amount = models.IntegerField()
+    energy = models.DecimalField(max_digits=6 , decimal_places=2)
+
+    def __str__(self):
+        return self.description
+
+    def get_owner_object(self):
+        '''
+        Returns the object that has owner information
+        '''
+        return self.plan
+
+    @staticmethod
+    def get_energy_value():
+        value = Nutrition.objects.aggregate(sum=Sum('energy'))['sum']
+
+        return value
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ nutrition/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render
+from .forms import PlanForm, NutritionForm
+from django.shortcuts import get_object_or_404
+from django.views import generic
+from nutrition.models import Plan, Nutrition
+from django.db.models import Q
+from django.template.context_processors import csrf
+from django.views.generic import (
+    UpdateView
+)
+from django.utils.translation import ugettext_lazy
+
+
+
+
+
+
+def create_plan(request):
+    if not request.user.is_authenticated():
+        return render(request, '../../accounts/templates/accounts/login.html')
+    else:
+        form = PlanForm(request.POST or None)
+        if form.is_valid():
+            plan = form.save(commit=False)
+            plan.user = request.user
+            plan.save()
+            return render(request, 'nutrition/detail.html', {'plan': plan})
+        context = {
+            "form": form,
+        }
+        return render(request, 'nutrition/create_plan.html', context)
+
+
+def create_nutrition(request, plan_id):
+    form = NutritionForm(request.POST or None)
+    plan = get_object_or_404(Plan, pk=plan_id)
+    if form.is_valid():
+        plans_nutritions = plan.nutrition_set.all()
+        for s in plans_nutritions:
+            if s.description == form.cleaned_data.get("description"):
+                context = {
+                    'plan': plan,
+                    'form': form,
+                    'error_message': 'You already added that description',
+                }
+                return render(request, 'nutrition/create_nutrition.html', context)
+        nutrition = form.save(commit=False)
+        nutrition.plan = plan
+        nutrition.save()
+        return render(request, 'nutrition/detail.html', {'plan': plan})
+    context = {
+        'plan': plan,
+        'form': form,
+    }
+    return render(request, 'nutrition/create_nutrition.html', context)
+
+
+def delete_plan(request, plan_id):
+    plan = Plan.objects.get(pk=plan_id)
+    plan.delete()
+    plans = Plan.objects.filter(user=request.user)
+    return render(request, 'nutrition/plan_list.html', {'plans': plans})
+
+
+def delete_nutrition(request, plan_id, nutrition_id):
+    plan = get_object_or_404(Plan, pk=plan_id)
+    nutrition = Nutrition.objects.get(pk=nutrition_id)
+    nutrition.delete()
+    return render(request, 'nutrition/detail.html', {'plan': plan})
+
+
+def detail(request, plan_id):
+    if not request.user.is_authenticated():
+        return render(request, '../../accounts/templates/accounts/login.html')
+    else:
+        plan = get_object_or_404(Plan, pk=plan_id)
+        user = plan.user
+        return render(request, 'nutrition/detail.html', {'plan': plan, 'user': user})
+
+
+def plan_list(request):
+    if not request.user.is_authenticated():
+        return render(request, '../../accounts/templates/accounts/login.html')
+    else:
+        plans = Plan.objects.filter(user=request.user)
+        nutrition_results = Nutrition.objects.all()
+        query = request.GET.get("q")
+        if query:
+            plans = plans.filter(
+                Q(plan_title__icontains=query) |
+                Q(subtitle__icontains=query)
+            ).distinct()
+            nutrition_results = nutrition_results.filter(
+                Q(nutrition_description__icontains=query)
+            ).distinct()
+            return render(request, 'nutrition/plan_list.html', {
+                'plans': plans,
+                'nutritions': nutrition_results,
+            })
+        else:
+            return render(request, 'nutrition/plan_list.html', {'plans': plans})
+
+
+
+
+
+
+
+
+
+
+
