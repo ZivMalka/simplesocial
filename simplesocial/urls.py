f61c69fa2b9eb18eead4ecc36e259c4ffeaba67d
BarAzulay <41300710+BarAzulay@users.noreply.github.com>
2018-08-11 22:40:17 +0300

    Update urls.py

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -1,18 +1,4 @@
-"""simplesocial URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.conf.urls import url,include
 from django.contrib import admin
@@ -29,5 +15,7 @@ urlpatterns = [
     url(r"^groups/", include('groups.urls', namespace='groups')),
     url(r"^activities/", include('activities.urls', namespace='activities')),
     url(r"^nutrition/", include('nutrition.urls', namespace='nutrition')),
+    url(r"^workout/", include('workout.urls', namespace='workout')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
