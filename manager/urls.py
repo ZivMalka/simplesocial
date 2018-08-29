1799aa52d0d2ff6e81c4155de1903f4770701325
BarAzulay <41300710+BarAzulay@users.noreply.github.com>
2018-08-29 13:29:20 +0300

    Update urls.py

--- manager/urls.py
+++ manager/urls.py
@@ -3,16 +3,10 @@ from . import views
 
 app_name = 'manager'
 
-
-
-
-
-
 urlpatterns = [
     url(r"menu/$", views.manager_control, name="manager"),
     url(r"workout/(?P<username>[-\w]+)/$", views.work_list, name="work_list"),
     url(r"plans(?P<username>[-\w]+)/$", views.plan_list, name="list"),
-    url(r'^results/$', views.search, name="search")
 ]
 
 
