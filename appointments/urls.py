1114c08b0d443709e115733fbf54529791a93e6e
BarAzulay <41300710+BarAzulay@users.noreply.github.com>
2018-08-30 08:50:27 +0300

    Update urls.py

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import (url)
-
 from appointments import views
 app_name = 'appointments'
 
@@ -7,6 +6,7 @@ urlpatterns = [
     url(r"menu/(?P<username>[-\w]+)/$", views.appoint, name='appoint'),
     url(r'^create_event$', views.create_event, name='create_event'),
     url(r'^(?P<appoint_id>[0-9]+)/delete_event/$', views.delete_event, name='delete_event'),
+    url(r'^(?P<appointment_id>[0-9]+)/edit_event/$', views.edit_event, name='edit_event'),
 
 
 
