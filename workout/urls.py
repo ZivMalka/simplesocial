f8243ab0e0d83ff2a73eeb3992399237d981b438
BarAzulay <41300710+BarAzulay@users.noreply.github.com>
2018-08-30 11:13:21 +0300

    Update urls.py

--- workout/urls.py
+++ workout/urls.py
@@ -5,15 +5,14 @@ app_name = 'workout'
 
 urlpatterns = [
     url(r"menu/(?P<username>[-\w]+)/$", views.overview, name="overview"),
-    url(r"menu/$", views.manageView, name="manageView"),
     url(r'^add_workout/$', views.add_workout, name='add_workout'),
     url(r'^(?P<workout_id>[0-9]+)/add_set/$', views.add_set, name='add_set'),
-    url(r'^(?P<workout_id>[0-9]+)/$', views.view, name='view'),
-    url(r'^(?P<workout_id>[0-9]+)/delete_workout/$', views.delete_workout, name='delete_workout'),
+    url(r'^(?P<workout_id>[0-9]+)/view/$', views.view, name='view'),
+    url(r'^(?P<workout_id>[0-9]+)/delete_workout/(?P<username>[-\w]+)/$', views.delete_workout, name='delete_workout'),
     url(r'^(?P<workout_id>[0-9]+)/delete_set/(?P<set_id>[0-9]+)/$', views.delete_set, name='delete_set'),
-    url(r'^(?P<workout_id>[0-9]+)/edit_set/(?P<set_id>[0-9]+)/$', views.edit_set, name='edit_set'),
-    url(r'^(?P<workout_id>[0-9]+)/edit_workout/$', views.edit_workout, name='edit_workout'),
-    url(r"workout/(?P<username>[-\w]+)/$", views.work_list_manage, name="work_list"),
+    url(r'^(?P<workout_id>[0-9]+)/edit_set/(?P<set_id>[0-9]+)/(?P<username>[-\w]+)/$', views.edit_set, name='edit_set'),
+    url(r'^(?P<workout_id>[0-9]+)/edit_workout/(?P<username>[-\w]+)/$', views.edit_workout, name='edit_workout'),
+    url(r"workout/(?P<username>[-\w]+)/$", views.work_list_manage, name="work_list_manage"),
 
 ]
 
