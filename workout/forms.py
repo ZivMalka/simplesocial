c11c6ea174892516adaef6bd7ee7432be5ff5a5a
BarAzulay <41300710+BarAzulay@users.noreply.github.com>
2018-08-30 09:14:59 +0300

    Update forms.py

--- workout/forms.py
+++ workout/forms.py
@@ -1,10 +1,12 @@
 from .models import Workout, Set
 from django import forms
 from emoji_picker.widgets import EmojiPickerTextInput, EmojiPickerTextarea
+from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget
+from django.forms.fields import DateField
 
 class WorkoutForm(forms.ModelForm):
     title = forms.CharField(widget=EmojiPickerTextInput)
-    creation_date = forms.DateTimeField(required=True, widget=forms.TextInput(attrs={"id": "datepicker"}))
+    creation_date = DateField(widget = AdminDateWidget())
     class Meta:
         model = Workout
         fields = ['user', 'day', 'title' , 'creation_date']
